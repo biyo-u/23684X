0ce44910453a63e51befcafb23d31f97c460d33d
biyo-u <biyouvbiama@gmail.com>
2024-12-01 16:34:08 -0700

    Refactored EagleFlow to use field-centric drive
    Changed EagleFlow to use field-centric drive for smoother movement.
    Added CURRENTHEADING for more accurate movement.
    Changed HEADING input to TARGETHEADING to reflect functionality changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/DriveMovements.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/DriveMovements.java
@@ -40,18 +40,24 @@ public class DriveMovements {
      * Stop: FL = 0, FR = 0, RL = 0, RR = 0
      * @param X The power applied to the motors, X
      * @param Y The power applied to the motors, Y
-     * @param HEADING The power applied to the motors, HEADING
+     * @param TARGETHEADING The power applied to the motors, TARGETHEADING
      */
-    public void EagleFlow(double X, double Y, double HEADING){
-        X = (X * modifier) * 1.1;
+    public void EagleFlow(double X, double Y, double TARGETHEADING){
+        X = X * modifier;
         Y = -Y * modifier;
-        HEADING = HEADING * modifier;
+        TARGETHEADING = TARGETHEADING * modifier;
+        double CURRENTHEADING = robot.compass.getHeading();
 
-        double denominator = Math.max(Math.abs(Y) + Math.abs(X) + Math.abs(HEADING), 1);
-        double frontLeftPower = (Y + X + HEADING) / denominator;
-        double backLeftPower = (Y - X + HEADING) / denominator;
-        double frontRightPower = (Y - X - HEADING) / denominator;
-        double backRightPower = (Y + X - HEADING) / denominator;
+        double rotX = X * Math.cos(-CURRENTHEADING) - Y * Math.sin(-CURRENTHEADING);
+        double rotY = X * Math.sin(-CURRENTHEADING) + Y * Math.cos(-CURRENTHEADING);
+
+        rotX = rotX * 1.1;
+
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(TARGETHEADING), 1);
+        double frontLeftPower = (Y + X + TARGETHEADING) / denominator;
+        double backLeftPower = (Y - X + TARGETHEADING) / denominator;
+        double frontRightPower = (Y - X - TARGETHEADING) / denominator;
+        double backRightPower = (Y + X - TARGETHEADING) / denominator;
 
         robot.drive.getFrontLeft().setPower(frontLeftPower);
         robot.drive.getFrontRight().setPower(frontRightPower);
