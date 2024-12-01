0f20e27c363e34263372ca45f601caf26810ddf8
biyo-u <biyouvbiama@gmail.com>
2024-12-01 15:51:49 -0700

    Refactored AutoDriver.java
    - Changed indentation to spaces instead of tabs.
    - Added a new method to move the robot to a specific position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/AutoDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EagleMatrix/AutoDriver.java
@@ -7,36 +7,36 @@ import org.firstinspires.ftc.teamcode.Subsystems.GoBildaPinpoint;
 import org.firstinspires.ftc.teamcode.Utilities.Position;
 
 public class AutoDriver {
-	Robot robot;
-	GoBildaPinpoint odometry;
-	DriveMovements driveMovements;
-	boolean initialized = false;
+    Robot robot;
+    GoBildaPinpoint odometry;
+    DriveMovements driveMovements;
+    boolean initialized = false;
 
-	// speed of robot = sin(((target location - current location)/target location) * pi) * speed modifier
-	// y = sin(xπ) * speed modifier (0.6) (MODIFIER MUST BE LESS OR EQUAL TO 1)
-	// repeat for yX, yY, and yHEADING
-	public enum MoveOrder {
-		HEADING_X_Y,
-		HEADING_Y_X,
-		X_HEADING_Y,
-		Y_HEADING_X,
-		X_Y_HEADING,
-		Y_X_HEADING,
-	}
+    // speed of robot = sin(((target location - current location)/target location) * pi) * speed modifier
+    // y = sin(xπ) * speed modifier (0.6) (MODIFIER MUST BE LESS OR EQUAL TO 1)
+    // repeat for yX, yY, and yHEADING
+    public enum MoveOrder {
+        HEADING_X_Y,
+        HEADING_Y_X,
+        X_HEADING_Y,
+        Y_HEADING_X,
+        X_Y_HEADING,
+        Y_X_HEADING,
+    }
 
-    public AutoDriver(Robot robot, GoBildaPinpoint odometry){
-		this.robot = robot;
-		this.odometry = odometry;
-		this.driveMovements = new DriveMovements(robot);
-	}
+    public AutoDriver(Robot robot, GoBildaPinpoint odometry) {
+        this.robot = robot;
+        this.odometry = odometry;
+        this.driveMovements = new DriveMovements(robot);
+    }
 
-	public boolean moveTo(Position position){
-		odometry.update();
-		Pose2D currentPosition = odometry.getPosition();
+    public boolean moveTo(Position position) {
+        odometry.update();
+        Pose2D currentPosition = odometry.getPosition();
 
-		if (!initialized){
+        if (!initialized) {
 
-			if (position.getVector2D().getY() == currentPosition.getY(DistanceUnit.INCH)){
+            if (position.getVector2D().getY() == currentPosition.getY(DistanceUnit.INCH)){
 				// No movement needed, may need to rotate later
 			} else if (position.getVector2D().getY() > currentPosition.getY(DistanceUnit.INCH)){
 				// Move forward
