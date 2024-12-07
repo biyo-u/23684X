9eee5c1ee5c336aaf66b65623241669cbd5b5dfd
Aarav Sharma <iamcoder18@gmail.com>
2024-12-07 09:09:10 -0700

    Reindent spaces to tabs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Compass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Compass.java
@@ -10,58 +10,58 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import java.util.Locale;
 
 public class Compass {
-    private final IMU imu;
-    private final GoBildaPinpointDriver odocomp;
+	private final IMU imu;
+	private final GoBildaPinpointDriver odocomp;
 
-    private String model;
+	private String model;
 
-    public Compass(IMU imu) {
-        this.imu = imu;
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                RevHubOrientationOnRobot.UsbFacingDirection.UP));
-        imu.initialize(parameters);
-        odocomp = null;
-    }
+	public Compass(IMU imu) {
+		this.imu = imu;
+		IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+				RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+				RevHubOrientationOnRobot.UsbFacingDirection.UP));
+		imu.initialize(parameters);
+		odocomp = null;
+	}
 
-    public double getHeading() {
-        if (odocomp != null) {
-            model = "Pinpoint IMU";
-            odocomp.update();
-            return odocomp.getHeading();
-        } else {
-            model = "REV Hub IMU";
-            return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        }
-    }
-    public void resetYaw() {
-       if (imu != null) {
-           imu.resetYaw();
-       } else {
-           odocomp.update();
-           Pose2D prevLocation = odocomp.getPosition();
-           odocomp.resetPosAndIMU();
-           odocomp.setPosition(new Pose2D(DistanceUnit.INCH, prevLocation.getX(DistanceUnit.INCH), prevLocation.getY(DistanceUnit.INCH),AngleUnit.DEGREES,0));
-       }
-    }
+	public double getHeading() {
+		if (odocomp != null) {
+			model = "Pinpoint IMU";
+			odocomp.update();
+			return odocomp.getHeading();
+		} else {
+			model = "REV Hub IMU";
+			return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+		}
+	}
+	public void resetYaw() {
+		if (imu != null) {
+			imu.resetYaw();
+		} else {
+			odocomp.update();
+			Pose2D prevLocation = odocomp.getPosition();
+			odocomp.resetPosAndIMU();
+			odocomp.setPosition(new Pose2D(DistanceUnit.INCH, prevLocation.getX(DistanceUnit.INCH), prevLocation.getY(DistanceUnit.INCH),AngleUnit.DEGREES,0));
+		}
+	}
 
-    public Compass(GoBildaPinpointDriver goBildaPinpointDriver) {
-        this.odocomp = goBildaPinpointDriver;
-        imu = null;
-    }
+	public Compass(GoBildaPinpointDriver goBildaPinpointDriver) {
+		this.odocomp = goBildaPinpointDriver;
+		imu = null;
+	}
 
-    public String getTelemetry() {
-        if (imu != null) {
-            return String.format(Locale.getDefault(), """
+	public String getTelemetry() {
+		if (imu != null) {
+			return String.format(Locale.getDefault(), """
                     Robot Yaw: %f
                     Robot Pitch: %f
                     Robot Roll: %f
                     Model Used: %s""", imu.getRobotYawPitchRollAngles().getYaw(), imu.getRobotYawPitchRollAngles().getPitch(), imu.getRobotYawPitchRollAngles().getRoll(), model);
-        } else {
-            odocomp.update();
-            return String.format(Locale.getDefault(), """
+		} else {
+			odocomp.update();
+			return String.format(Locale.getDefault(), """
                 Robot Yaw: %f
                 Model Used: %s""", odocomp.getHeading(), model);
-        }
-    }
+		}
+	}
 }
