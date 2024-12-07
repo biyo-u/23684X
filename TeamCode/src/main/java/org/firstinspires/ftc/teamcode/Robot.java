a629c92647460ffdfe3c1ccb6f1db63437efc58f
Aarav Sharma <iamcoder18@gmail.com>
2024-12-07 09:37:43 -0700

    Updated Gradle and Android Gradle Plugin versions.
    Enabled configuration cache and pickFirsts for JNI libs.
    
    Removed redundant packaging options and updated robot initialization to use GoBildaPinpointDriver for odometry and compass.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -41,24 +41,21 @@ public class Robot {
         DcMotor rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
         DcMotor rearRight = hardwareMap.get(DcMotor.class, "rearRight");
 
-        IMU imu = hardwareMap.get(IMU.class, "imu");
         GoBildaPinpointDriver odometryComputer = hardwareMap.get(GoBildaPinpointDriver.class, "odometry");
 
         // Initialize Public Subsystems
-//        compass = new Compass(imu);
         compass = new Compass(odometryComputer);
         intake = new Intake(clawFront, wristServo, clawBack, elbowMotor, spinWrist, wristBackServo);
         lift = new Lift(liftMotorLeft, liftMotorRight, shoulderMotor, liftServoTilt, rightHangServo, leftHangServo);
         drive = new Drive(frontLeft, frontRight, rearLeft, rearRight, compass);
 
 
+        // Public Devices (not subsystems)
         this.odometry = hardwareMap.get(GoBildaPinpointDriver.class, "odometry");
         this.odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
         this.odometry.setOffsets(-6.44, 6.8745);
         this.odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
         this.odometry.resetPosAndIMU();
         this.odometry.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
-
-//        odometry = new Odometry(odometryComputer, compass);
     }
 }
