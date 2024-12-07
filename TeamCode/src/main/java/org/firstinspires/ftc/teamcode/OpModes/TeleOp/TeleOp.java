632c92cc2d738384e40d19764717d4aab8055d8f
Aarav Sharma <iamcoder18@gmail.com>
2024-12-07 09:04:22 -0700

    Replaced spaces with tabs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOp.java
@@ -6,98 +6,98 @@ import org.firstinspires.ftc.teamcode.Robot;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp", group = Constants.GroupNames.TeleOp)
 public class TeleOp extends OpMode {
-    private Robot robot;
+	private Robot robot;
 
-    @Override
-    public void init() {
-        robot = new Robot(hardwareMap);
-    }
+	@Override
+	public void init() {
+		robot = new Robot(hardwareMap);
+	}
 
-    @Override
-    public void loop() {
-        // Drive the robot with the game-pad
-        robot.drive.driveMecanumFieldCentric(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+	@Override
+	public void loop() {
+		// Drive the robot with the game-pad
+		robot.drive.driveMecanumFieldCentric(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
-        // Reset IMU for Field Centric
-        if (gamepad1.left_bumper) {
-            robot.compass.resetYaw();
-        }
+		// Reset IMU for Field Centric
+		if (gamepad1.left_bumper) {
+			robot.compass.resetYaw();
+		}
 
-        // Set speed mode
-        if (gamepad1.right_bumper) {
-            robot.drive.setPower(1);
-        } else {
-            robot.drive.setPower(0.6);
-        }
+		// Set speed mode
+		if (gamepad1.right_bumper) {
+			robot.drive.setPower(1);
+		} else {
+			robot.drive.setPower(0.6);
+		}
 
-        // Front Wrist
-        if (gamepad2.x){
-            robot.intake.wristUp();
-        } else if (gamepad2.b) {
-            robot.intake.wristDown();
-        }
+		// Front Wrist
+		if (gamepad2.x){
+			robot.intake.wristUp();
+		} else if (gamepad2.b) {
+			robot.intake.wristDown();
+		}
 
-        // Back Wrist
-        if (gamepad2.y) {
-            robot.intake.wristBackUp();
-        } else if (gamepad2.a) {
-            robot.intake.wristBackDown();
-        }
+		// Back Wrist
+		if (gamepad2.y) {
+			robot.intake.wristBackUp();
+		} else if (gamepad2.a) {
+			robot.intake.wristBackDown();
+		}
 
-        // Lift
-        robot.lift.liftMove(gamepad2.left_stick_y);
+		// Lift
+		robot.lift.liftMove(gamepad2.left_stick_y);
 
-        // Lift Tilt
-        if (gamepad2.left_stick_x == 1){
-            robot.lift.liftTiltBack();
-        } else if (gamepad2.left_stick_x == -1){
-            robot.lift.liftTIltStraight();
-        }
+		// Lift Tilt
+		if (gamepad2.left_stick_x == 1){
+			robot.lift.liftTiltBack();
+		} else if (gamepad2.left_stick_x == -1){
+			robot.lift.liftTIltStraight();
+		}
 
-        // Shoulder
-        robot.lift.shoulderMove(gamepad2.right_stick_x);
+		// Shoulder
+		robot.lift.shoulderMove(gamepad2.right_stick_x);
 
-        // Hang Hooks
-        if (gamepad2.dpad_up) {
-            robot.lift.hang(1, 0);
-        } else if (gamepad2.dpad_down) {
-            robot.lift.hang(0.1, 0.7);
-        }
+		// Hang Hooks
+		if (gamepad2.dpad_up) {
+			robot.lift.hang(1, 0);
+		} else if (gamepad2.dpad_down) {
+			robot.lift.hang(0.1, 0.7);
+		}
 
-        //  Front Claw
-        if (gamepad2.right_trigger > 0) {
-            robot.intake.clawOpen();
-        } else {
-            robot.intake.clawClose();
-        }
+		//  Front Claw
+		if (gamepad2.right_trigger > 0) {
+			robot.intake.clawOpen();
+		} else {
+			robot.intake.clawClose();
+		}
 
-        // Back Claw
-        if (gamepad2.left_trigger > 0) {
-            robot.intake.clawBackOpen();
-        } else {
-            robot.intake.clawBackClose();
-        }
+		// Back Claw
+		if (gamepad2.left_trigger > 0) {
+			robot.intake.clawBackOpen();
+		} else {
+			robot.intake.clawBackClose();
+		}
 
-        // spin Back Claw
-        if (gamepad2.dpad_left) {
-            robot.intake.spinWristStraight();
-        } else if (gamepad2.dpad_right) {
-            robot.intake.spinWristSideways();
-        }
-        // Elbow Motor
-        if (gamepad2.right_stick_y > 0) {
-            robot.intake.ElbowMotorUp();
-        } else if (gamepad2.right_stick_y < 0) {
-            robot.intake.ElbowMotorDown();
-        }
+		// spin Back Claw
+		if (gamepad2.dpad_left) {
+			robot.intake.spinWristStraight();
+		} else if (gamepad2.dpad_right) {
+			robot.intake.spinWristSideways();
+		}
+		// Elbow Motor
+		if (gamepad2.right_stick_y > 0) {
+			robot.intake.ElbowMotorUp();
+		} else if (gamepad2.right_stick_y < 0) {
+			robot.intake.ElbowMotorDown();
+		}
 
-        robot.odometry.update();
+		robot.odometry.update();
 
-        // Telemetry
-        telemetry.addLine(robot.lift.getTelemetry());
-        telemetry.addLine(robot.lift.getJointLiftPosition());
-        telemetry.addLine(robot.intake.getTelemetry());
-        telemetry.addLine(robot.odometry.getPosition().toString());
-        telemetry.addLine(robot.compass.getTelemetry());
-    }
+		// Telemetry
+		telemetry.addLine(robot.lift.getTelemetry());
+		telemetry.addLine(robot.lift.getJointLiftPosition());
+		telemetry.addLine(robot.intake.getTelemetry());
+		telemetry.addLine(robot.odometry.getPosition().toString());
+		telemetry.addLine(robot.compass.getTelemetry());
+	}
 }
