babccd1d2a6d7d4a3f2b441c42535d6cb5c0b188
Aarav Sharma <iamcoder18@gmail.com>
2024-12-07 08:58:18 -0700

    Updated code formatting in Constants.java to use tabs instead of spaces.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -3,27 +3,27 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Pose2d;
 
 public class Constants {
-    public static final boolean developerMode = true;
+	public static final boolean developerMode = true;
 
-    // TODO: Find these values
-    public static final int liftLeftForwardLimit = 8333;
-    public static final int liftRightForwardLimit = 8013;
-    public static final int liftForwardLimit = 8013;
-    public static final int liftBackwardLimit = -999999;
-    public static final int shoulderForwardLimit = -999999;
-    public static final int shoulderBackwardLimit = 9999999;
-    public static final double odometryWeight = 0.2;
-    // TODO: Map values from threshold to 1/-1 for precise movement even with a dead zone
-    public static final double liftThreshold = 0.2;
-    public static final double shoulderThreshold = 0.2;
-    public static final Pose2d RedObservationZone = new Pose2d(16, 60, Math.toRadians(90));
+	// TODO: Find these values
+	public static final int liftLeftForwardLimit = 8333;
+	public static final int liftRightForwardLimit = 8013;
+	public static final int liftForwardLimit = 8013;
+	public static final int liftBackwardLimit = -999999;
+	public static final int shoulderForwardLimit = -999999;
+	public static final int shoulderBackwardLimit = 9999999;
+	public static final double odometryWeight = 0.2;
+	// TODO: Map values from threshold to 1/-1 for precise movement even with a dead zone
+	public static final double liftThreshold = 0.2;
+	public static final double shoulderThreshold = 0.2;
+	public static final Pose2d RedObservationZone = new Pose2d(16, 60, Math.toRadians(90));
 
-    public static final double doubleErrorThreshold = 0.00001;
+	public static final double doubleErrorThreshold = 0.00001;
 
-    public static class GroupNames {
-        public static final String TeleOp = "1.TeleOp";
-        public static final String Autonomous = "1.Auto";
-        public static final String RoadrunnerTuning = "9.RoadrunnerTuning";
-        public static final String Testing = "8.Testing";
-    }
+	public static class GroupNames {
+		public static final String TeleOp = "1.TeleOp";
+		public static final String Autonomous = "1.Auto";
+		public static final String RoadrunnerTuning = "9.RoadrunnerTuning";
+		public static final String Testing = "8.Testing";
+	}
 }
